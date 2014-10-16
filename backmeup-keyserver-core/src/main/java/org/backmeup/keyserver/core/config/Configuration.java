656a3fa39fcb04520bf089fc65dc47abf7cee442
keros <ft@x-net.at>
2014-10-16 15:28:52 +0200

    added configuration via properties file

---
+++ backmeup-keyserver-core/src/main/java/org/backmeup/keyserver/core/config/Configuration.java
@@ -0,0 +1,36 @@
+package org.backmeup.keyserver.core.config;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public final class Configuration {
+	private static final Properties properties = new Properties();
+	
+	private static final String PROPERTYFILE = "backmeup-keyserver.properties";
+	
+	static {
+		try {
+			ClassLoader loader = Thread.currentThread().getContextClassLoader();
+			if (loader.getResourceAsStream(PROPERTYFILE) != null) {
+				properties.load(loader.getResourceAsStream(PROPERTYFILE));
+			} else {
+				throw new IOException("unable to load properties file: " + PROPERTYFILE);
+			}
+		} catch (IOException e) {
+			throw new ExceptionInInitializerError(e);
+		}
+	}
+
+	public static String getProperty(String key) {
+		return properties.getProperty(key);
+	}
+	
+	public static String getProperty(String key, String defaultValue) {
+		String value = getProperty(key);
+		if (value != null) {
+			return value;
+		} else {
+			return defaultValue;
+		}
+	}
+}
