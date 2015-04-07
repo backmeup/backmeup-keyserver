2fdd477510da2ea0907f3b808b4a1bcd1e442e89
Wolfgang Eibner <we@x-net.at>
2015-04-07 20:12:52 +0200

    more RESTful way for URL pathes, user profile set/get, user remove, password change

--- backmeup-keyserver-rest/src/main/java/org/backmeup/keyserver/rest/resources/Users.java
+++ backmeup-keyserver-rest/src/main/java/org/backmeup/keyserver/rest/resources/Users.java
@@ -5,8 +5,11 @@ import javax.validation.constraints.NotNull;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.backmeup.keyserver.core.KeyserverException;
@@ -31,65 +34,63 @@ public class Users extends SecureBase {
 
     @RolesAllowed("CORE")
     @POST
-    @Path("/authenticate/")
+    @Path("/authenticate")
     public AuthResponseDTO authenticate(@NotNull @FormParam("username") String username, @NotNull @FormParam("password") String password)
             throws KeyserverException {
         return getMapper().map(this.getKeyserverLogic().authenticateUserWithPassword(username, password), AuthResponseDTO.class);
     }
 
+    @RolesAllowed("CORE")
+    @TokenRequired
+    @DELETE
+    @Path("/{serviceUserId}")
+    public void remove(@PathParam("serviceUserId") String serviceUserId) throws KeyserverException {
+        AuthResponse auth = this.getAuthResponse();
+        getKeyserverLogic().removeUser(auth.getServiceUserId(), auth.getUsername(), auth.getAccountKey());
+    }
+
+    @RolesAllowed("CORE")
+    @DELETE
+    @Path("/{serviceUserId}/adminRemove")
+    public void adminRemove(@PathParam("serviceUserId") String serviceUserId, @NotNull @QueryParam("username") String username)
+            throws KeyserverException {
+        getKeyserverLogic().removeUser(serviceUserId, username);
+    }
+
     @RolesAllowed({ "CORE", "INDEXER" })
     @TokenRequired
     @GET
-    @Path("/index_key/")
-    public String getIndexKey() throws KeyserverException {
+    @Path("/{serviceUserId}/index_key")
+    public String getIndexKey(@PathParam("serviceUserId") String serviceUserId) throws KeyserverException {
         AuthResponse auth = this.getAuthResponse();
         return this.getKeyserverLogic().getIndexKey(auth.getUserId(), auth.getAccountKey());
     }
-    
-/*
-    @RolesAllowed("CORE")
+
+    @RolesAllowed({ "CORE", "INDEXER", "WORKER" })
+    @TokenRequired
     @GET
-    @Path("/")
-    public List<AppDTO> listApps() {
-        List<AppDTO> appList = new ArrayList<>();
-        try {
-            List<App> apps = getKeyserverLogic().listApps(this.getApp().getPassword());
-            for (App a : apps) {
-                appList.add(getMapper().map(a, AppDTO.class));
-            }
-        } catch(KeyserverException e) {
-            throw new WebApplicationException(e, Status.INTERNAL_SERVER_ERROR);
-        }
-        return appList;
+    @Path("/{serviceUserId}/profile")
+    public String getProfile(@PathParam("serviceUserId") String serviceUserId) throws KeyserverException {
+        AuthResponse auth = this.getAuthResponse();
+        return this.getKeyserverLogic().getProfile(auth.getUserId(), auth.getAccountKey());
     }
-    
 
-    
-    @RolesAllowed("CORE")
-    @DELETE
-    @Path("/{appId}/")
-    public void remove(@PathParam("appId") String appId) {
-        try {
-            this.getKeyserverLogic().removeApp(appId);
-        } catch(EntryNotFoundException e) {
-            throw new WebApplicationException(e, Status.NOT_FOUND);
-        } catch(KeyserverException e) {
-            throw new WebApplicationException(e, Status.INTERNAL_SERVER_ERROR);
-        }
-    }
-    
-    @RolesAllowed({"CORE", "WORKER", "STORAGE", "INDEXER"})
+    @RolesAllowed({ "CORE" })
+    @TokenRequired
     @POST
-    @Path("/{appId}/")
-    public void authenticate(@PathParam("appId") String appId, @NotNull @FormParam("key") String appKey) {
-        try {
-            this.getKeyserverLogic().authenticateApp(appId, appKey);
-        } catch(EntryNotFoundException e) {
-            throw new WebApplicationException(e, Status.NOT_FOUND);
-        } catch(KeyserverException e) {
-            throw new WebApplicationException(e, Status.UNAUTHORIZED);
-        }
+    @Path("/{serviceUserId}/profile")
+    public void setProfile(@PathParam("serviceUserId") String serviceUserId, @NotNull @FormParam("profile") String profile) throws KeyserverException {
+        AuthResponse auth = this.getAuthResponse();
+        this.getKeyserverLogic().setProfile(auth.getUserId(), auth.getAccountKey(), profile);
     }
-*/
 
+    @RolesAllowed({ "CORE" })
+    @TokenRequired
+    @POST
+    @Path("/{serviceUserId}/changePassword")
+    public void changePassword(@PathParam("serviceUserId") String serviceUserId, @NotNull @FormParam("oldPassword") String oldPassword,
+            @NotNull @FormParam("newPassword") String newPassword) throws KeyserverException {
+        AuthResponse auth = this.getAuthResponse();
+        this.getKeyserverLogic().changeUserPassword(auth.getUserId(), auth.getUsername(), oldPassword, newPassword);
+    }
 }
